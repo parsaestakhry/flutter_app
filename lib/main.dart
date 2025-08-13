bf96e213d940ab3da691f96b791e5054ffa8c80e
Parsa Estakhry <parsaestakhry03@gmail.com>
2025-08-13 16:16:11 +0330

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -95,10 +95,14 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Image.network(
-        "https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        height: double.infinity,
-        fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Image.network(
+            "https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            fit: BoxFit.cover,
+          ),
+          Text('Hello World from flutter '),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
