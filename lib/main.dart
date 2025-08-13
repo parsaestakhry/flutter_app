a0ea216fa911c39a1e8878593daf131fae58358d
Parsa Estakhry <parsaestakhry03@gmail.com>
2025-08-13 16:47:04 +0330

    ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -103,14 +103,12 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: EdgeInsets.all(50.0),
         child: Stack(
           children: [
-            Image.network(
-              "https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-              fit: BoxFit.cover,
-              height: 300.0,
-            ),
-            SizedBox(
-              child: Center(child: Text('Hello World from flutter ')),
-              height: 300.0,
+            ListTile(
+              tileColor: Colors.red,
+              title: Text("helloooo"),
+              leading: Icon(Icons.abc),
+              trailing: Text("twe0"),
+              onTap: () {},
             ),
           ],
         ),
