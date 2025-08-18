df424c0eb65689f9385f837748ec405de3c35488
Parsa Estakhry <parsaestakhry03@gmail.com>
2025-08-18 15:29:14 +0330

    gesture detector

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -69,8 +69,13 @@ class _ProfilePageState extends State<ProfilePage> {
                 });
               },
             ),
-            Image.network(
-              "https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            GestureDetector(
+              onTap: () {
+                print("hello");
+              },
+              child: Image.network(
+                "https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              ),
             ),
           ],
         ),
