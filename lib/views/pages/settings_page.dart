82d5f60a655fde9aaf860f39d2ad12c3d039becc
Parsa Estakhry <parsaestakhry03@gmail.com>
2025-08-19 13:58:04 +0330

    pass data through pages

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({super.key, required this.title,});
+  final String title;
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -16,7 +17,7 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Settings Page")),
+      appBar: AppBar(title: Text(widget.title)),
 
       body: SingleChildScrollView(
         child: Padding(
